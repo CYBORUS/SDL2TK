731b9ee80fe6a6a586cf94b1dbe1a312e5b9d462
TheBuzzSaw <thebuzzsaw@gmail.com>
2013-10-04 23:30:16 -0600

    Porting matrix class.

--- sample/TestLibUbu64/main.cpp
+++ sample/TestLibUbu64/main.cpp
@@ -1,8 +1,19 @@
 #include "TestModule.hpp"
 #include <SDL2TK/Window.hpp>
+#include <SDL2TK/Matrix4x4.hpp>
+#include <iostream>
+using namespace std;
 
 int main(int argc, char** argv)
 {
+    SDL2TK::Rotation<double> rotation
+        = SDL2TK::Rotation<double>::FromDegrees(180.0);
+    SDL2TK::Matrix4x4<double> m;
+
+    rotation = rotation + rotation;
+
+    cout << rotation.ToRadians() << endl;
+
     SDL_Init(SDL_INIT_VIDEO);
     {
         SDL2TK::Window window;
